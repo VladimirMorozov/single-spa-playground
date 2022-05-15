dfcc07aa6d87326f0a718532ce304896058f021c
Vladimir Morozov <vladimir@ringit.ee>
2022-05-15 21:11:01 +0300

    routing

--- header/src/root.component.js
+++ header/src/root.component.js
@@ -1,3 +1,15 @@
+import { Link, BrowserRouter } from "react-router-dom";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  window.mountCount = window.mountCount || 0;
+  window.mountCount++;
+  return <>
+    <BrowserRouter>
+    <p>Count: {window.mountCount}</p>
+    <section>{props.name} is mounted!</section>
+    <Link to={"app1"}>app 1</Link>
+    <br />
+    <Link to={"app2"}>app 2</Link>
+    </BrowserRouter>
+  </>;
 }
