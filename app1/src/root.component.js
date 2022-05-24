a5e3c6f3e9775d2bc05dd8f55c51da414a340cf4
Vladimir Morozov <vladimir.morozov@ringit.ee>
2022-05-24 11:17:12 +0300

    check picture support

--- app1/src/root.component.js
+++ app1/src/root.component.js
@@ -1,6 +1,8 @@
 import { SubComponent } from "./SubComponent";
 import * as React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import logo from './logo.svg';
+import pic from './pic.jpg';
 
 export default function Root(props) {
   return <BrowserRouter basename="private/app1">
@@ -9,5 +11,7 @@ export default function Root(props) {
         <Route path="/" element={<section>{props.name} is mounted!</section>} />
         <Route path="about" element={<SubComponent />} />
       </Routes>
+      <img src={logo} className="App-logo" alt="logo" />
+      <img src={pic} />
   </BrowserRouter>
 }
